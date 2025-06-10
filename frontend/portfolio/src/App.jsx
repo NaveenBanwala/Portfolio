95dd2218f7632a804f7cbb011d01438cb447cbc4
NaveenBanwala <nbanwala7@gmail.com>
2025-06-10 18:06:52 +0530

    Build the Entity,Services,Repository and use docker for postgres database

---
+++ frontend/portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+    return(
+        <>
+        <div className="text-green-700"></div>
+            <h1>Welcome to My Portfolio</h1>
+            </>
+    );
+}
+export default App;
