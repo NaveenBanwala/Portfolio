c12af8fd75142b065b989189ac9aaea1cb770cc9
Naveen Banwala <nbanwala7@gmail.com>
2025-06-10 18:22:01 +0530

    Test commit to verify contribution tracking

--- frontend/portfolio/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App(){
-    return(
-        <>
-        <div className="text-green-700"></div>
-            <h1>Welcome to My Portfolio</h1>
-            </>
-    );
-}
-export default App;
